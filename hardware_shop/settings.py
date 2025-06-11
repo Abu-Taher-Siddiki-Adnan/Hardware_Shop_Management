3021a16f90dfee1d9c1e800ee0b10ca728159bf1
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2025-06-11 12:25:28 +0600

    Updated

--- hardware_shop/settings.py
+++ hardware_shop/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-6v1iiam6ml1+-k_h3r)4&p(jzhkt5yd$7&*j7a6vm^7jgfiid^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
